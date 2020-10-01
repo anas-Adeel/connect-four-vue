a5448ee72eb9159344ef2047cf8511c337733105
Anas Adeel <68690808+anas-Adeel@users.noreply.github.com>
2020-10-01 12:12:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-console.log("HELLO WORLD PLS READ ME")
-console.log("THIS IS THE FIRST ANDSECOND LINES OF CODE IN THE NPM SCRIPT")
-
-
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 
 const app = express();
 
-app.use("/", serveStatic(path.join(__dirname, "./dist")));
-
 const port = process.env.PORT || 8080;
+
+app.all("/", serveStatic(path.join(__dirname, "./dist")));
+app.get("/", serveStatic(path.join(__dirname, "./dist")));
+app.use("/", serveStatic(path.join(__dirname, "./dist")));
 app.listen(port);
 
 console.log(`Listening on port : ${port}`);
